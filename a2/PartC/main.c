48a9590c0b137eddbb8756f0ad0febe6b5a8abcd
Johannes Hirschbeck <johannes@Johanness-MacBook.local>
2025-07-13 10:44:57 +0200

    finished assignment 2 and started assignment 3

--- a2/PartC/main.c
+++ a2/PartC/main.c
@@ -8,28 +8,49 @@
 #include "VirtualSerial.h"
 #include "packetizer.h"
 #include "crypto.h"
+#include "sodium.h"
+#include "system_XMC4500.h"
 
-int main(void) {
-  uint8_t key[8] = {0x42, 0x42, 0x42, 0x42, 0x42, 0x42, 0x42, 0x42};
-  pt_s plaintext = { 0 };
-  ct_s ciphertext = { 0 };
+int main(void)
+{
+  sodium_init();
+  int j = 0;
+
+  uint8_t key[32];
+  for (int i = 1; i < 32; i++)
+  {
+    key[i] = g_chipid[j];
+    j++;
+    if (j == 16)
+    {
+      j = 0;
+    }
+  }
+
+  pt_s plaintext = {0};
+  ct_s ciphertext = {0};
 
   USB_Init();
 
-  while(1) {
+  while (1)
+  {
     /* Consumes incoming data and parses it until we received a valid packet.
        If so, it will fill the plaintextJob struct with the appropriate information. */
-    if(packetizerReceive(&plaintext) == 0) {
+    if (packetizerReceive(&plaintext) == 0)
+    {
       /* If the sender did not sent a nonce, generate one */
-      if(plaintext.nonce == NULL) {
+      if (plaintext.nonce == NULL)
+      {
         plaintext.nonce = malloc(CRYPTO_NONCE_BYTES);
-        for(size_t i = 0; i < CRYPTO_NONCE_BYTES; i++) {
+        for (size_t i = 0; i < CRYPTO_NONCE_BYTES; i++)
+        {
           plaintext.nonce[i] = rand() & UINT8_MAX;
         }
       }
       /* Now encrypt the plaintext */
-      if(encrypt(&ciphertext.text, plaintext.text, plaintext.textLen, plaintext.nonce,
-                 key) == 0) {
+      if (crypto_secretbox_easy(ciphertext.text, plaintext.text, plaintext.textLen, plaintext.nonce,
+                                key) == 0)
+      {
         ciphertext.textLen = getCiphertextLength(plaintext.textLen);
         /* Finally, if encryption was successful, send ciphertext back in a packet */
         packetizerSend(&ciphertext);
--- a2/PartC/student.mk
+++ a2/PartC/student.mk
@@ -8,11 +8,12 @@ LD_NAME = Assignment2C
 # Own sources, e.g. main.c
 SRCS = main.c clockSetup.c VirtualSerial.c Descriptors.c packetizer.c crypto.c base64url.c MPUeasy.c stack_protector.c
 # Header files for configuration. Adding the header file will  make make compile on changes.
-HDRS = VirtualSerial.h Descriptors.h packetizer.h crypto.h base64url.h
+HDRS = VirtualSerial.h Descriptors.h packetizer.h crypto.h base64url.h ../randombytes_salsa20XMC_random.h
+
 # Library sources, e.g. xmc_gpio.c
 LIBSRCS = USBController_XMC4000.c xmc_usbd.c CDCClassDevice.c USBTask.c Endpoint_XMC4000.c xmc4_scu.c EndpointStream_XMC4000.c memcpy.c xmc_gpio.c xmc4_gpio.c xmc_ccu4.c
 # Precompiled libraries, e.g. -lm for math functions
-LIBLNK =
+LIBLNK =  -L ../sodium/sodium-1.0.15/lib -lsodium
 
 # Change this line if the path to your XMC-Library differs, it will be overwritten before grading to
 # match the system the grading is performed on.
@@ -23,6 +24,8 @@ SCFLAGS  = -std=gnu99
 # Optimization level, remember that enabling optimization will stirr up the assembly code and thus
 # debugging is more difficult
 SCFLAGS += -O1
+SCFLAGS += -I ../sodium/sodium-1.0.15/include
+SCFLAGS += -I ../sodium/sodium-1.0.15/include/sodium
 # If you like, you can enable even more warnings, e.g. -Wextra, but for grading -Wall will be used
 # You can also add any other compiler flag here if either necessary or you find it appropriate
 SCFLAGS += -fstack-protector
